183d1ad5e032f200ec4f85fd62b60dea12610126
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-05-29 15:42:29 +0200

    Fix parallax and spike jittering

--- GMD-Project/Assets/Scripts/Parallax/ParallaxController.cs
+++ GMD-Project/Assets/Scripts/Parallax/ParallaxController.cs
@@ -6,12 +6,12 @@ namespace Parallax
 {
     public class ParallaxController : MonoBehaviour
     {
-        [SerializeField] private Transform cameraTransform;
         [SerializeField] private Sprite[] parallaxSprites;
         [SerializeField] private float parallaxSpeedX = 0.08f;
         [SerializeField] private float parallaxSpeedY = 0.01f;
+        [SerializeField] private float cameraOffsetY = 2f;
         [SerializeField] private float repeatThreshold = 18f;
-        
+
         private readonly List<GameObject> _parallaxGameObjects = new();
         private void Start()
         {
@@ -32,16 +32,13 @@ namespace Parallax
             }
         }
         
-        private void LateUpdate()
+        private void Update()
         {
-            // Make parent follow camera
-            transform.position = new Vector3(cameraTransform.position.x, cameraTransform.position.y, transform.position.z);
-            
             // Adjust position of parallax layers relative to parent object
             for (var i = 0; i < _parallaxGameObjects.Count; i++)
             {
                 var parallaxOffsetX = ((transform.position.x * (parallaxSpeedX * (_parallaxGameObjects.Count - i)) + repeatThreshold) % (repeatThreshold * 2) - repeatThreshold) * -1;
-                var parallaxOffsetY = transform.position.y * (parallaxSpeedY * (_parallaxGameObjects.Count - i)) * -1;
+                var parallaxOffsetY = transform.position.y * (parallaxSpeedY * (_parallaxGameObjects.Count - i)) * -1 + cameraOffsetY;
                 _parallaxGameObjects[i].transform.localPosition = new Vector3(parallaxOffsetX, parallaxOffsetY, _parallaxGameObjects[i].transform.position.z);
             }
         }
