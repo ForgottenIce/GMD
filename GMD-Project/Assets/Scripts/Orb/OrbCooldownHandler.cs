f7a82251c02367607734c2b95d7a8017572519ae
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-06-04 19:12:52 +0200

    Orb Refactor
    Made orb responsible for its respawn cooldown instead of the player. If the player died, the orb was unable to respawn. Now it doesn't depend on the player being alive.

---
+++ GMD-Project/Assets/Scripts/Orb/OrbCooldownHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using Player;
+using UnityEngine;
+
+namespace Orb
+{
+    public class OrbCooldownHandler : MonoBehaviour
+    {
+        [SerializeField] private float orbCooldown;
+        
+        public void ApplyCooldown(GameObject orbInstance)
+        {
+            StartCoroutine(TemporarilyDisableOrb(orbInstance));
+        }
+        
+        private IEnumerator TemporarilyDisableOrb(GameObject orbInstance)
+        {
+            orbInstance.SetActive(false);
+            yield return new WaitForSeconds(orbCooldown);
+            orbInstance.SetActive(true);
+        }
+    }
+}
---
+++ GMD-Project/Assets/Scripts/Orb/OrbInstance.cs
@@ -0,0 +1,33 @@
+﻿using Player;
+using UnityEngine;
+
+namespace Orb
+{
+    public class OrbInstance : MonoBehaviour
+    {
+        private OrbCooldownHandler _orbCooldownHandler;
+        
+        private void Start()
+        {
+            _orbCooldownHandler = GetComponentInParent<OrbCooldownHandler>();
+        }
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var playerMovementStateMachine = other.GetComponent<PlayerMovementStateMachine>();
+            
+            if (playerMovementStateMachine != null)
+            {
+                if (gameObject.CompareTag("JumpOrb"))
+                {
+                    playerMovementStateMachine.JumpOrbCollected();
+                }
+                if (gameObject.CompareTag("DashOrb"))
+                {
+                    playerMovementStateMachine.DashOrbCollected();
+                }
+                
+                _orbCooldownHandler.ApplyCooldown(gameObject);
+            }
+        }
+    }
+}
