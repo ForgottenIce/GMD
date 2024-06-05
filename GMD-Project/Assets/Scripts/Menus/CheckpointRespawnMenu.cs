993e874a6668533dde59f4f9bc0f251b78c42361
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-06-05 09:18:21 +0200

    Update Menus & Add Checkpoint
    Updated pause menu to have a respawn at checkpoint button
    Updated respawn menu to not have an exit button, since you could hit it by accident
    Added a checkpoint before first slime enemy

--- GMD-Project/Assets/Scripts/Menus/CheckpointRespawnMenu.cs
+++ GMD-Project/Assets/Scripts/Menus/CheckpointRespawnMenu.cs
@@ -9,23 +9,21 @@ namespace Menus
     {
         [SerializeField] private CheckpointManager checkpointManager;
         [SerializeField] private InputManager inputManager;
-        
+
         private GameObject _checkpointRespawnUi;
         private Button _respawnButton;
-        private Button _exitButton;
-        
+
         private AudioSource _audioSource;
 
         private void Start()
         {
             _checkpointRespawnUi = transform.GetChild(0).gameObject;
-            
+
             _respawnButton = _checkpointRespawnUi.transform.GetChild(0).GetComponent<Button>();
-            _exitButton = _checkpointRespawnUi.transform.GetChild(1).GetComponent<Button>();
-            
+
             _audioSource = GetComponent<AudioSource>();
         }
-        
+
         public void ShowMenu()
         {
             _checkpointRespawnUi.SetActive(true);
@@ -33,17 +31,12 @@ namespace Menus
             _audioSource.Play();
             _respawnButton.Select();
         }
-        
+
         public void RespawnAtCurrentCheckpoint()
         {
             _checkpointRespawnUi.SetActive(false);
             inputManager.PauseButtonDisabled = false;
             checkpointManager.RestartAtCurrentCheckpoint();
-        }
-
-        public void ExitGame()
-        {
-            Application.Quit();
         }
     }
 }
--- GMD-Project/Assets/Scripts/Menus/PauseMenu.cs
+++ GMD-Project/Assets/Scripts/Menus/PauseMenu.cs
@@ -1,3 +1,4 @@
+using Checkpoint;
 using Input;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -9,6 +10,7 @@ namespace Menus
     public class PauseMenu : MonoBehaviour
     {
         [SerializeField] private InputManager inputManager;
+        [SerializeField] private CheckpointManager checkpointManager;
         private bool _isPaused;
         private GameObject _pauseUi;
         private Button _resumeButton;
@@ -18,7 +20,7 @@ namespace Menus
             _pauseUi = transform.GetChild(0).gameObject;
             _resumeButton = _pauseUi.transform.GetChild(0).GetComponent<Button>();
         }
-        
+
         private void Update()
         {
             if (inputManager.PausePressed && !_isPaused)
@@ -31,27 +33,33 @@ namespace Menus
                 ResumeGame();
             }
         }
-        
+
         public void PauseGame()
         {
             _isPaused = true;
             Time.timeScale = 0f;
             _pauseUi.SetActive(true);
         }
-        
+
         public void ResumeGame()
         {
             _isPaused = false;
             Time.timeScale = 1f;
             _pauseUi.SetActive(false);
         }
-        
+
+        public void RespawnAtCurrentCheckpoint()
+        {
+            ResumeGame();
+            checkpointManager.RestartAtCurrentCheckpoint();
+        }
+
         public void RestartGame()
         {
             ResumeGame();
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
-        
+
         public void ExitGame()
         {
             Application.Quit();
